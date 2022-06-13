7da90ca693ad55afa3e77e6305c86113e4d6c162
Asif <asif.md@dollarbirdinc.com>
2022-06-13 20:00:41 +0530

    solution and swiper section updated

--- Hexaware Screen/assets/js/custom.js
+++ Hexaware Screen/assets/js/custom.js
@@ -8,26 +8,33 @@ $().ready(function () {
 });
 
 var swiper = new Swiper(".mySwiper", {
+  speed: 500,
+  loop: false,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
 
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 249) {
-      $(".header").addClass("fadeOutLeft");
-      $(".header").removeClass("fadeInLeft");
-      $(".header-sticky").addClass("fadeInDown");
-      $(".header-sticky").removeClass("fadeOutUp");
-      $(".header-sticky").addClass("animated");
-    } else {
-      $(".header").addClass("fadeInLeft");
-      $(".header").removeClass("fadeOutLeft");
-      $(".header-sticky").addClass("fadeOutUp");
-      $(".header-sticky").removeClass("fadeInUp");
-    }
-  });
-});
+// $(function () {
+//   var scroll = $(document).scrollTop();
+//   var navHeight = $(".nav-area").outerHeight();
+//   $(window).scroll(function () {
+//     var scrolled = $(document).scrollTop();
+//     if (scrolled > navHeight) {
+//       $(".nav-area").addClass("animate");
+//     } else {
+//       $("nav-area").removeClass("animate");
+//     }
+//     if (scrolled > scroll) {
+//       $("nav-area").removeClass("sticky");
+//     } else {
+//       $$("nav-area").addClass("sticky");
+//     }
+//     scroll = $(document).scrollTop();
+//   });
+// });
