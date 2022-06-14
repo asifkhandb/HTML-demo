af03f32cca3151cc041bd4f4fdf48e1e52d67798
Asif <asif.md@dollarbirdinc.com>
2022-06-14 20:55:17 +0530

    second slider update

--- Hexaware Screen/assets/js/custom.js
+++ Hexaware Screen/assets/js/custom.js
@@ -20,21 +20,43 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// $(function () {
-//   var scroll = $(document).scrollTop();
-//   var navHeight = $(".nav-area").outerHeight();
-//   $(window).scroll(function () {
-//     var scrolled = $(document).scrollTop();
-//     if (scrolled > navHeight) {
-//       $(".nav-area").addClass("animate");
-//     } else {
-//       $("nav-area").removeClass("animate");
-//     }
-//     if (scrolled > scroll) {
-//       $("nav-area").removeClass("sticky");
-//     } else {
-//       $$("nav-area").addClass("sticky");
-//     }
-//     scroll = $(document).scrollTop();
-//   });
-// });
+//  for sticky navbar
+
+var new_scroll_position = 0;
+var last_scroll_position;
+var header = document.getElementById("header");
+
+window.addEventListener("scroll", function (e) {
+  last_scroll_position = window.scrollY;
+
+  // Scrolling down
+  if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+    // header.removeClass('slideDown').addClass('slideUp');
+    header.classList.remove("slideDown");
+    header.classList.add("slideUp");
+
+    // Scrolling up
+  } else if (new_scroll_position > last_scroll_position) {
+    // header.removeClass('slideUp').addClass('slideDown');
+    header.classList.remove("slideUp");
+    header.classList.add("slideDown");
+  }
+
+  new_scroll_position = last_scroll_position;
+});
+
+// for sticky navbar
+
+// second swiper slider
+var swiper = new Swiper(".mySwiperSecond", {
+  slidesPerView: 2.2,
+  spaceBetween: 35,
+  slidesPerGroup: 3,
+  loop: false,
+  loopFillGroupWithBlank: true,
+  watchSlidesVisibility: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
